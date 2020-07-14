f10aa2e032d62c09aebb8bccb11e3bb804bac313
Roman Zipp <ich@ich.wtf>
2020-07-14 22:24:14 +0200

    Update abstract listener class to allow subscribing to event attributes

--- app/Support/Listeners/AbstractListener.php
+++ app/Support/Listeners/AbstractListener.php
@@ -7,6 +7,8 @@ use Illuminate\Support\Arr;
 
 abstract class AbstractListener
 {
+    protected array $subscribe = [];
+
     public function listen(Dispatcher $dispatcher, string $event): void
     {
         $dispatcher->listen(
@@ -18,5 +20,22 @@ abstract class AbstractListener
     protected function getClassFromNamespace(string $namespace): string
     {
         return Arr::last(explode('\\', $namespace));
+    }
+
+    public function subscribe(Dispatcher $dispatcher): void
+    {
+        foreach ($this->subscribe as $event => $callback) {
+
+            if (is_array($callback)) {
+
+                if (count($callback) === 1) {
+                    $callback = [static::class, $callback[0]];
+                }
+
+                $callback = implode('@', $callback);
+            }
+
+            $dispatcher->listen($event, $callback);
+        }
     }
 }

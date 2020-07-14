684b0a5dba0ffabfd18670f50f1774c1254d8ed8
Roman Zipp <ich@ich.wtf>
2020-07-14 23:08:00 +0200

    Add seo provider & Replace template head tags with seo

---
+++ app/App/Providers/SeoServiceProvider.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\ServiceProvider;
+use romanzipp\Seo\Helpers\Hook;
+use romanzipp\Seo\Structs;
+
+class SeoServiceProvider extends ServiceProvider
+{
+    public function boot(): void
+    {
+        Structs\Title::hook(
+            Hook::make()
+                ->onBody()
+                ->callback(function ($body) {
+                    return ($body ? $body . ' | ' : '') . config('app.name');
+                })
+        );
+
+        seo()->charset();
+        seo()->viewport();
+
+        seo()->title('Home');
+        seo()->description('My Description');
+
+        seo()->addMany([
+
+            Structs\Meta::make()->name('copyright')->content('Roman Zipp'),
+
+            Structs\Meta::make()->name('mobile-web-app-capable')->content('yes'),
+            Structs\Meta::make()->name('theme-color')->content('#f03a17'),
+
+            Structs\Link::make()->rel('icon')->href('/assets/images/Logo.png'),
+
+            Structs\Meta\OpenGraph::make()->property('site_name')->content(config('app.name')),
+            Structs\Meta\OpenGraph::make()->property('locale')->content(config('app.locale')),
+
+        ]);
+    }
+}
--- resources/views/app/layouts/app.blade.php
+++ resources/views/app/layouts/app.blade.php
@@ -1,15 +1,14 @@
 <!doctype html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    {{ seo()->render() }}
 
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
-
     <script src="{{ manifest('js/app.js') }}" defer></script>
     <link href="{{ manifest('css/app.css') }}" rel="stylesheet">
+
 </head>
 <body>
 

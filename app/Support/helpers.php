81353c65536eecf30421aee99dcb287fb896cc55
Roman Zipp <ich@ich.wtf>
2020-07-14 22:26:43 +0200

    Update abstract data logic

--- app/Support/helpers.php
+++ app/Support/helpers.php
@@ -9,3 +9,19 @@ if ( ! function_exists('manifest')) {
         return Manifest::make()->manifest($manifest)->url($path, $absolute);
     }
 }
+
+if ( ! function_exists('property_set')) {
+
+    /**
+     * Check if a class property has been set with any value. in contrast to isset(), if set
+     * with NULL this function will return FALSE.
+     *
+     * @param $class
+     * @param string $property
+     * @return bool
+     */
+    function property_set($class, string $property): bool
+    {
+        return array_key_exists($property, get_object_vars($class));
+    }
+}

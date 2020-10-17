d276f6e47022fd5c63a35a15bc312163960eed7d
Roman Zipp <ich@ich.wtf>
2020-10-17 17:05:30 +0200

    Update exceptions handler to process api responses

--- app/App/Exceptions/Handler.php
+++ app/App/Exceptions/Handler.php
@@ -2,7 +2,10 @@
 
 namespace App\Exceptions;
 
+use Illuminate\Auth\AuthenticationException;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Illuminate\Http\Exceptions\HttpResponseException;
+use Illuminate\Http\Request;
 use Throwable;
 
 class Handler extends ExceptionHandler
@@ -23,6 +26,45 @@ class Handler extends ExceptionHandler
 
     public function render($request, Throwable $exception)
     {
-        return parent::render($request, $exception);
+        if ( ! $this->isApiCall($request)) {
+            return parent::render($request, $exception);
+        }
+
+        $exception = $this->prepareException($exception);
+
+        if ($exception instanceof AuthenticationException) {
+            return $this->unauthenticated($request, $exception);
+        }
+
+        if ($exception instanceof HttpResponseException) {
+            return $exception->getResponse();
+        }
+
+        return $this->prepareJsonResponse($request, $exception);
+    }
+
+    /**
+     * Convert an authentication exception into a response.
+     *
+     * @param \Illuminate\Http\Request $request
+     * @param \Illuminate\Auth\AuthenticationException $exception
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    protected function unauthenticated($request, AuthenticationException $exception)
+    {
+        return $request->expectsJson() || $this->isApiCall($request)
+            ? response()->json(['message' => $exception->getMessage()], 401)
+            : redirect()->guest($exception->redirectTo() ?? route('login'));
+    }
+
+    /**
+     * Determines if request is an api call.
+     *
+     * @param Request $request
+     * @return boolean
+     */
+    protected function isApiCall(Request $request): bool
+    {
+        return $request->is('api') || $request->is('api/*');
     }
 }

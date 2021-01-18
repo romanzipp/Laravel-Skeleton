f33b25bcf77eb35804a9fa141d47a1e1edb95357
Roman Zipp <ich@ich.wtf>
2021-01-18 07:21:49 +0100

    Update abstract job default attributes & Queue monitor helpers

--- app/Support/Jobs/AbstractJob.php
+++ app/Support/Jobs/AbstractJob.php
@@ -15,7 +15,61 @@ abstract class AbstractJob implements ShouldQueue
     use Queueable;
     use SerializesModels;
 
+    /**
+     * The number of times the job may be attempted.
+     *
+     * @var int
+     */
+    public int $tries = 1;
+
+    /**
+     * The number of seconds to wait before retrying the job.
+     *
+     * @var int
+     */
+    public int $backoff = 30;
+
+    /**
+     * The number of seconds the job can run before timing out.
+     *
+     * @var int
+     */
     public int $timeout = 0;
 
     abstract public function handle();
+
+    protected function withMemoryData(): void
+    {
+        if ( ! method_exists($this, 'queueData')) {
+            return;
+        }
+
+        $this->queueData([
+            '_memory' => $this->getMemoryUsage(),
+        ], true);
+    }
+
+    protected function withServerName(): void
+    {
+        if ( ! method_exists($this, 'queueData')) {
+            return;
+        }
+
+        $this->queueData([
+            '_server' => $this->getServerName(),
+        ], true);
+    }
+
+    protected function getServerName()
+    {
+        return gethostname();
+    }
+
+    protected function getMemoryUsage(): array
+    {
+        return [
+            'current' => memory_get_usage(),
+            'peak' => memory_get_peak_usage(),
+        ];
+    }
 }

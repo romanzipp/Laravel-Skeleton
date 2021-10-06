a574f9ae6511b2dec476d110ac18dfa990d8a3d6
Roman Zipp <ich@ich.wtf>
2021-10-06 09:16:11 +0200

    Publish model doc package config

---
+++ config/model-doc.php
@@ -0,0 +1,33 @@
+<?php
+
+return [
+    'relations' => [
+        // Enable generating PHPDoc properties for relation methods
+        // @property MyRelatedModel|null $my_relation
+        'enabled' => true,
+
+        'counts' => [
+            // Enable generating PHPDoc properties for relations count attributes
+            // @property int|null $my_relation_counts
+            'enabled' => true,
+        ],
+
+        'base_model' => \Support\Models\AbstractModel::class,
+    ],
+
+    'attributes' => [
+        // Enable generating PHPDoc properties for database columns
+        'enabled' => true,
+    ],
+
+    'scopes' => [
+        // Enable generating model query scope methods
+        'enabled' => true,
+        // Define certain scope methods that should be ignored (provide final method name without "scope" prefix)
+        'ignore' => [
+            'whereUuid',
+        ],
+    ],
+
+    'fail_when_empty' => false,
+];

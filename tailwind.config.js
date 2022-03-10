287e97ded76cb834fa654adfd58446bbb2d459e0
Roman Zipp <ich@ich.wtf>
2022-03-10 15:13:15 +0100

    Upgrade npm packages

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,11 @@ const form = require('./resources/js/tailwind/form');
 const button = require('./resources/js/tailwind/button');
 
 module.exports = {
+    content: [
+        './resources/**/*.blade.php',
+        './resources/**/*.js',
+        './resources/**/*.vue',
+    ],
     theme: {
         extend: { },
     },
--- webpack.mix.js
+++ webpack.mix.js
@@ -1,6 +1,8 @@
 const mix = require('laravel-mix');
 const tailwindcss = require('tailwindcss');
 
+mix.disableNotifications();
+
 mix.js('resources/js/app.js', 'public/js');
 
 mix.sass('resources/sass/app.scss', 'public/css')

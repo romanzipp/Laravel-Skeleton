32beb55cd728c7f36d04f5152b49488a82787b43
Roman Zipp <ich@ich.wtf>
2022-03-10 14:46:44 +0100

    Remove UUID traits from pivot base model

--- app/Support/Models/AbstractPivotModel.php
+++ app/Support/Models/AbstractPivotModel.php
@@ -2,18 +2,12 @@
 
 namespace Support\Models;
 
-use Dyrynda\Database\Support\GeneratesUuid;
 use Illuminate\Database\Eloquent\Relations\Pivot;
 use Support\Models\Concerns\InteractsWithTable;
-use Support\Models\Concerns\UsesUuidPrimaryKey;
 
 abstract class AbstractPivotModel extends Pivot
 {
     use InteractsWithTable;
-    use UsesUuidPrimaryKey;
-    use GeneratesUuid {
-        UsesUuidPrimaryKey::uuidColumn insteadof GeneratesUuid;
-    }
 
     public $incrementing = false;
 

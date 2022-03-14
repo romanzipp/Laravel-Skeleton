f9e386e2278b80e467d9a3ce689ea1e94cf6abee
Roman Zipp <ich@ich.wtf>
2022-03-14 19:03:54 +0100

    Download gravatar on user creation

--- app/Domain/User/Actions/CreateUser.php
+++ app/Domain/User/Actions/CreateUser.php
@@ -5,6 +5,7 @@ namespace Domain\User\Actions;
 use Carbon\Carbon;
 use Domain\User\Data\CreateUserData;
 use Domain\User\Models\User;
+use Spatie\MediaLibrary\MediaCollections\Exceptions\UnreachableUrl;
 
 final class CreateUser
 {
@@ -17,6 +18,14 @@ final class CreateUser
             'password' => $data->password,
             'terms_accepted_at' => Carbon::now(),
         ]);
+
+        try {
+            $user
+                ->addMediaFromUrl(sprintf('https://www.gravatar.com/avatar/%s.jpg?d=404&s=200', md5(strtolower(trim($data->email)))))
+                ->toMediaCollection('avatar');
+        } catch (UnreachableUrl $exception) {
+            report($exception);
+        }
 
         $user->save();
 

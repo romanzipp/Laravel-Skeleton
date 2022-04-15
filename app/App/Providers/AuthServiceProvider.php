655acdccbf5c94d45e8ef5ddd97779abe05219d2
Roman Zipp <ich@ich.wtf>
2022-04-15 21:18:06 +0200

    Integrate passport

--- app/App/Providers/AuthServiceProvider.php
+++ app/App/Providers/AuthServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
+use Laravel\Passport\Passport;
 
 class AuthServiceProvider extends ServiceProvider
 {
@@ -22,5 +23,9 @@ class AuthServiceProvider extends ServiceProvider
     public function boot()
     {
         $this->registerPolicies();
+
+        if ( ! $this->app->routesAreCached()) {
+            Passport::routes();
+        }
     }
 }

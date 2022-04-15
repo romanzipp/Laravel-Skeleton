aff882c57d0a378cc32b894d362929df78f115f5
Roman Zipp <ich@ich.wtf>
2022-04-15 21:08:23 +0200

    Add laravel passport

--- app/Support/Enums/TableName.php
+++ app/Support/Enums/TableName.php
@@ -7,6 +7,12 @@ class TableName extends AbstractEnum
     public const SUPPORT_JOBS = 'support-jobs';
     public const SUPPORT_FAILED_JOBS = 'support-failed_jobs';
 
+    public const OAUTH_AUTH_CODES = 'oauth__auth_codes';
+    public const OAUTH_ACCESS_TOKENS = 'oauth__access_tokens';
+    public const OAUTH_REFRESH_TOKENS = 'oauth__refresh_tokens';
+    public const OAUTH_CLIENTS = 'oauth__clients';
+    public const OAUTH_PERSONAL_ACCESS_CLIENTS = 'oauth__personal_access_clients';
+
     public const USER_USERS = 'user-users';
     public const USER_PASSWORD_RESETS = 'user-password_resets';
 

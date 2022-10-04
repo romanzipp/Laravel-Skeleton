c8db9817ca0a1b9064aac747cc7c7890bbbba46b
Roman Zipp <ich@ich.wtf>
2022-10-04 15:34:43 +0200

    Update eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,35 +5,19 @@ module.exports = {
     },
     extends: [
         'airbnb-base',
-        'plugin:vue/essential',
-        'plugin:vue/strongly-recommended',
     ],
     parserOptions: {
         parser: 'babel-eslint',
         sourceType: 'module',
         ecmaVersion: 12,
     },
-    plugins: [
-        'vue',
-    ],
     rules: {
         indent: ['error', 4],
         'max-len': 'off',
-        'import/no-unresolved': 'off',
+        'no-continue': 'off',
         'no-restricted-syntax': 'off',
-        'vue/html-indent': ['error', 4],
-        'vue/html-closing-bracket-newline': ['error', {
-            singleline: 'never',
-            multiline: 'never',
-        }],
-        'vue/multiline-html-element-content-newline': ['error', {
-            allowEmptyLines: true,
-        }],
-        'vue/max-attributes-per-line': ['error', {
-            multiline: {
-                allowFirstLine: true,
-            },
-        }],
+        'import/no-unresolved': 'off',
+        'import/no-extraneous-dependencies': 'off',
     },
     settings: {
         'import/resolver': 'webpack',

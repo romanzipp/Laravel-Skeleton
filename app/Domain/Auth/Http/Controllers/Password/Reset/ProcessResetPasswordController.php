3c6f5f85198e52d730fcca359caf8c13fcd222cf
Roman Zipp <ich@ich.wtf>
2022-10-06 09:14:39 +0200

    Add turnstile captcha to password reset route

--- app/Domain/Auth/Http/Controllers/Password/Reset/ProcessResetPasswordController.php
+++ app/Domain/Auth/Http/Controllers/Password/Reset/ProcessResetPasswordController.php
@@ -5,6 +5,8 @@ namespace Domain\Auth\Http\Controllers\Password\Reset;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Foundation\Auth\ResetsPasswords;
 use Illuminate\Http\Request;
+use Illuminate\Validation\Rules\Password;
+use romanzipp\Turnstile\Rules\TurnstileCaptcha;
 
 final class ProcessResetPasswordController
 {
@@ -15,5 +17,28 @@ final class ProcessResetPasswordController
     public function __invoke(Request $request)
     {
         return $this->reset($request);
+    }
+
+    protected function rules()
+    {
+        return [
+            'token' => [
+                'required',
+            ],
+            'email' => [
+                'required',
+                'email',
+            ],
+            'password' => [
+                'required',
+                'confirmed',
+                Password::defaults(),
+            ],
+            'cf-turnstile-response' => config('turnstile.site_key') ? [
+                'required',
+                'string',
+                new TurnstileCaptcha(),
+            ] : [],
+        ];
     }
 }
